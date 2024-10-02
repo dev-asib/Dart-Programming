205904a019072ee3335124766004927a49d7f892
dev-asib <mail.asib.com@gmail.com>
2024-10-02 23:01:03 +0600

    Updated Special Types Runes

---
+++ 03.Advanced/01.SpecialTypes/06.Runes/runes.dart
@@ -0,0 +1,15 @@
+void main() {
+  String smallA = "a";
+  String capitalA = "A";
+  String name = "Asib";
+  print(smallA.runes);
+  print(capitalA.runes);
+  print(name.runes);
+
+  Runes myName = Runes(name);
+  print(myName);
+
+
+
+
+}
