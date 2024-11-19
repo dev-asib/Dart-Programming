1cc8e648d89b67639686da820492647a28b8122d
dev-asib <mail.asib.com@gmail.com>
2024-11-19 21:16:40 +0600

    Bitwise Operators

---
+++ 03.Advanced/02.Operators/02.CasecadeOperators/01.bitwise_operators.dart
@@ -0,0 +1,33 @@
+void main() {
+  // Bitwise and Shift Operators
+  // & (Bitwise AND): print(5 & 3); // 1
+  // | (Bitwise OR): print(5 | 3); // 7
+  // ^ (Bitwise XOR): print(5 ^ 3); // 6
+  // ~ (Bitwise NOT): print(~5); // -6
+  // << (Left shift): print(5 << 2); // 20
+  // >> (Right shift): print(5 >> 2); // 1
+
+  // "&" (Bitwise AND)
+  print("Bitwise AND (5 & 3) = ${5 & 3}");
+  print("Bitwise AND (8 & 4) = ${8 & 4}");
+
+  // "|" (Bitwise OR)
+  print("Bitwise OR(5 | 3) = ${5 | 3}");
+  print("Bitwise OR(8 | 4) = ${8 | 4}");
+
+  // "^" (Bitwise XOR)
+  print("Bitwise XOR(5 ^ 3) = ${5 ^ 3}");
+  print("Bitwise XOR(8 ^ 4) = ${8 ^ 4}");
+
+  // "~" (Bitwise NOT)
+  print("Bitwise NOT (~5) = ${~5}");
+  print("Bitwise NOT (~8) = ${~8}");
+
+  // "<<" Left Shift
+  print("Left Shift (5<<3) = ${5 << 1}");
+  print("Left Shift (8<<2) = ${8 << 2}");
+
+  // ">>" Right Shift
+  print("Right Shift (5>>3) = ${5 >> 1}");
+  print("Right Shift (8>>2) = ${8 >> 2}");
+}
